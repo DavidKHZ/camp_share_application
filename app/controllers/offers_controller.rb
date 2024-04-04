a96f9aec208bd8780b6e36f7f5584538907a3917
David Cahez <cahez.david@gmail.com>
2024-04-04 22:10:30 +1100

    Created the routes and basic controller functions for offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,50 @@
 class OffersController < ApplicationController
+  # to be done : index show new create edit update
+  def index
+    @offers = Offer.all
+    @offers = @offers.where(category: params[:category]) if params[:category]
+    @offers = @offers.where(city: params[:city]) if params[:city]
+    @offers = @offers.where(country: params[:country]) if params[:country]
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(
+      :user_id,
+      :name,
+      :category,
+      :pick_up_from,
+      :pick_up_till,
+      :return_from,
+      :return_till,
+      :country,
+      :state,
+      :city,
+      :postcode,
+      :address_1,
+      :address_2,
+      :description,
+      :available,
+      :price_per_day
+      )
+  end
 end
