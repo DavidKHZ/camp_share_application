3b9ff5126b553850110795916eee392ee5388c41
David Cahez <cahez.david@gmail.com>
2024-04-05 00:19:01 +1100

    stopped on the bug to fill in forms for creating new offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,4 +3,8 @@ class Offer < ApplicationRecord
   has_many :bookings
   has_many_attached :photos
   validates :photos, attached: true, content_type: [:png, :jpg, :jpeg]
+  validates :category, inclusion: {
+    in: CATEGORIES,
+    message: "must be one of #{CATEGORIES[0..-2].join(", ") + " or " + CATEGORIES[-1]}"
+  }
 end
