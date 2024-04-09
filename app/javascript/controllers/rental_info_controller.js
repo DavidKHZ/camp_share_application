d393d381ff9d5c8a3e029e1a06634f26e1973ef7
David Cahez <cahez.david@gmail.com>
2024-04-09 23:48:38 +1100

    removed console log from Stimulus library

--- app/javascript/controllers/rental_info_controller.js
+++ app/javascript/controllers/rental_info_controller.js
@@ -5,23 +5,23 @@ export default class extends Controller {
   static targets = ["from", "to", "text", "days", "price", "button", "rule"]
 
   connect() {
-    console.log("Rental info connected")
-    console.log("Day rate :", this.element.getAttribute("data-rental-info-dayrate"))
+    // console.log("Rental info connected")
+    // console.log("Day rate :", this.element.getAttribute("data-rental-info-dayrate"))
   }
 
   update () {
-    console.log("Update triggered")
+    // console.log("Update triggered")
     const from = this.stringToDate(this.fromTarget.value)
     const to = this.stringToDate(this.toTarget.value)
     const dayrate = this.element.getAttribute("data-rental-info-dayrate")
-    console.log("From :", from)
-    console.log("To :", to)
-    console.log("Dayrate :", dayrate)
+    // console.log("From :", from)
+    // console.log("To :", to)
+    // console.log("Dayrate :", dayrate)
     if (!isNaN(from) && !isNaN(to) && to >= from ) {
       this.buttonTarget.disabled = false;
       this.ruleTarget.classList.add("d-none")
       const days = this.rentalDays(from, to)
-      console.log(days)
+      // console.log(days)
       this.textTarget.classList.remove("d-none")
       this.daysTarget.innerText = days
       this.priceTarget.innerText = Math.round(days * dayrate*100) / 100
