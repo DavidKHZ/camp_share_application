1dd0864287d699a3a375f2d677722ce5e89c33dc
David Cahez <cahez.david@gmail.com>
2024-04-09 22:41:04 +1100

    added rental info to the booking

---
+++ app/javascript/controllers/rental_info_controller.js
@@ -0,0 +1,43 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="rental-info"
+export default class extends Controller {
+  static targets = ["from", "to", "text", "days", "price"]
+
+  connect() {
+    console.log("Rental info connected")
+    console.log("Day rate :", this.element.getAttribute("data-rental-info-dayrate"))
+  }
+
+  update () {
+    console.log("Update triggered")
+    const from = this.stringToDate(this.fromTarget.value)
+    const to = this.stringToDate(this.toTarget.value)
+    const dayrate = this.element.getAttribute("data-rental-info-dayrate")
+    console.log("From :", from)
+    console.log("To :", to)
+    console.log("Dayrate :", dayrate)
+    if (!isNaN(from) && !isNaN(to)) {
+      const days = this.rentalDays(from, to)
+      console.log(days)
+      this.textTarget.classList.remove("d-none")
+      this.daysTarget.innerText = days
+      this.priceTarget.innerText = Math.round(days * dayrate*100) / 100
+    }
+
+  }
+
+  stringToDate(strDate) {
+    const splitStrDate = strDate.split("-")
+    const year = parseInt(splitStrDate[0])
+    const month = parseInt(splitStrDate[1]) - 1
+    const day = parseInt(splitStrDate[2])
+
+    return new Date(Date.UTC(year, month, day))
+  }
+
+  rentalDays(from, to) {
+    const diffInMilliseconds = Math.abs(to - from);
+    return Math.ceil(1 + diffInMilliseconds / (1000 * 60 * 60 * 24));
+  }
+}
