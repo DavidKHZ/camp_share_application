d9360a9367e4004fa7115e71176c72dd18393ddb
David Cahez <cahez.david@gmail.com>
2024-04-11 19:40:46 +1100

    Route created, controller and erb file initiated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  def home
-    # @lists = List.all
+
+  def home; end
+
+  def dashboard
+    @submitted_bookings = Booking.where(user: current_user)
   end
 end
